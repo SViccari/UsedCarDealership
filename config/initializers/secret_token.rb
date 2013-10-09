e6450f571802b94de48866e4db8a43541ae0c636
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-10-09 13:54:54 -0400

    Initial commit. Rspec set-up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UsedCarLot::Application.config.secret_key_base = 'aacfd521c49c2c9da1b86837838e71a123d40c1f21a87397e5be64ec08155b77a73a637e8a1f677a4d56b185bb28cd2e8bd00bbf69cc34a4c9883e4010d1abe1'
